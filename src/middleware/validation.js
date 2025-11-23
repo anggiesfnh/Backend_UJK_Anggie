e78884efaf6e91e5a85e6d245ff8e0208aa332bd
anggie <anggiesfnh@gmail.com>
2025-11-23 14:21:33 +0700

    change validation

--- src/middleware/validation.js
+++ src/middleware/validation.js
@@ -1,10 +1,14 @@
-const validationBodySiswa = (req, res) => {
-    let { nama, alamat, tanggal_lahir, jurusan } = req.body;
-
-    if ( !nama || !alamat || !tanggal_lahir || !jurusan ) {
-        res.status(400).json({message: "nama, alamat, tanggal_lahir and jurusan is required"});
+const validationBodySiswa = (req, res, next) => {
+    const { nama, alamat, tanggal_lahir, jurusan } = req.body;
+    if (!nama || !alamat || !tanggal_lahir || !jurusan) {
+        return res.status(400).json({ 
+            message: "nama, alamat, tanggal_lahir and jurusan is required" 
+        });
     }
+
+    next();
 };
+
 module.exports = {
     validationBodySiswa
-}
+};
